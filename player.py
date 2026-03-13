cdd0739059e593c676e251f7b583cbcd2c79305b
Gaven <ghuynh@student.cscc.edu>
2026-03-12 21:29:17 -0400

    Import Coin class from coin.py

--- player.py
+++ player.py
@@ -5,3 +5,5 @@ This file defines the player class which represents the player.
 Each player will have an attached name, "wallet" and coin object they can toss.
 March 12, 2026
 """
+
+from coin import Coin
